e3024757ab30ca01c77a4a1f528c7710710c3a5c
Renato Machado <mehahx@gmail.com>
2018-04-12 01:31:11 -0300

    fix liveview

--- fw/js/events.js
+++ fw/js/events.js
@@ -5,5 +5,8 @@ window.onpopstate = function(event) {
 		emulateJSON: true
 	}).then(function(data) {
 		eval(data.body);
+		if(Vue.liveView) {
+			Vue.liveView.checkModification(location.pathname.substring(Vue.CONTEXT_PATH.length));
+		}
 	});
 };
